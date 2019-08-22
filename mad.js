cfeb8d19f64d952b321e1b4a06f99c184004d52c
Mrityunjay Singh <mrityunjay0510@gmail.com>
2019-08-22 23:23:13 +0530

    UI Changes

--- mad.js
+++ mad.js
@@ -1,7 +1,22 @@
 window.onload=function(){
 
-    var mainDiv = document.getElementById("mainDiv");
-    
+    var mainDiv = document.getElementById("maindiv");
 
+    showQualitySlide().changeSlide();
 
+}
+
+function showQualitySlide(){
+    var slideIndex=0;  
+    var qualities=['Single Application',' Verified Avenues','Trusted Advisors'];
+    changeSlide();
+
+    function changeSlide(){
+        var qualitydiv = document.getElementById("quality_div");
+        if(slideIndex===qualities.length){
+            slideIndex=0;
+        } 
+        qualitydiv.innerText=qualities[slideIndex++];
+        setTimeout(changeSlide,3000);
+    }
 }
